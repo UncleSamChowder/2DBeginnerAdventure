0a19afe92bdf8fabb48cf82e671a787eb5fdcdb0
UncleSamChowder <745603@student.dusd.net>
2024-11-14 11:46:44 -0800

    I added animation to robot and chuby.

--- 2DBeginnerAdventureCeperoP4Samuel/Assets/Scripts/ChubyController.cs
+++ 2DBeginnerAdventureCeperoP4Samuel/Assets/Scripts/ChubyController.cs
@@ -6,7 +6,7 @@ public class ChubyController : MonoBehaviour
 {
     public float speed = 4.0f;
     public int maxHealth = 5;
-    public float timeInvincible = 2;
+    public float timeInvincible = 2.0f;
     public int health { get { return currentHealth; } }
     int currentHealth;
 
@@ -16,11 +16,16 @@ public class ChubyController : MonoBehaviour
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
+    
     // Start is called before the first frame update
     void Start()
     {
        rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,6 +34,15 @@ public class ChubyController : MonoBehaviour
          horizontal = Input.GetAxis("Horizontal");
          vertical = Input.GetAxis("Vertical");
 
+         Vector2 move = new Vector2(horizontal, vertical);
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X",lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
         if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;

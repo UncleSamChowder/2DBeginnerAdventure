0b25d1c8bab7500918c0b0e0bf1bfb7e1a78d2ac
UncleSamChowder <745603@student.dusd.net>
2024-11-22 12:18:23 -0800

    I added smoke to my game.

--- 2DBeginnerAdventureCeperoP4Samuel/Assets/Scripts/EnemyController.cs
+++ 2DBeginnerAdventureCeperoP4Samuel/Assets/Scripts/EnemyController.cs
@@ -8,7 +8,8 @@ public class EnemyController : MonoBehaviour
     public float speed = 3.0f;
     public bool vertical;
     public float changeTime = 3.0f;
-    
+
+    public ParticleSystem smokeEffect;
     Rigidbody2D rigidbody2d;
 
     bool broken = true;
@@ -77,6 +78,7 @@ public class EnemyController : MonoBehaviour
         broken = false;
         rigidbody2d.simulated = false;
         animator.SetTrigger("Fixed");
+        smokeEffect.Stop();
     }
 
 }

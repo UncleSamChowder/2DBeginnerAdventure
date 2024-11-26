5ae87000cd3680c039f882ad1971016e084ad3b8
UncleSamChowder <745603@student.dusd.net>
2024-11-25 19:23:26 -0800

    I finished my project and uploaded.

--- 2DBeginnerAdventureCeperoP4Samuel/Assets/Scripts/ChubyController.cs
+++ 2DBeginnerAdventureCeperoP4Samuel/Assets/Scripts/ChubyController.cs
@@ -22,6 +22,10 @@ public class ChubyController : MonoBehaviour
 
     Animator animator;
     Vector2 lookDirection = new Vector2(1,0);
+
+    AudioSource audioSource;
+    public AudioClip throwSound;
+    public AudioClip hitSound;
     
     // Start is called before the first frame update
     void Start()
@@ -29,6 +33,8 @@ public class ChubyController : MonoBehaviour
        rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
         animator = GetComponent<Animator>();
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -58,6 +64,20 @@ public class ChubyController : MonoBehaviour
         {
             Launch();
         }
+
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if(hit.collider != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+            if(character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
+
+        }
        
     }
     void FixedUpdate()
@@ -80,9 +100,10 @@ public class ChubyController : MonoBehaviour
             }
             isInvincible = true;
             invincibleTimer = timeInvincible;
+            PlaySound(hitSound); 
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth); 
+        UIHealthBar.instance.SetValue(currentHealth/(float)maxHealth); 
     }
 
     void Launch()
@@ -93,6 +114,13 @@ public class ChubyController : MonoBehaviour
         projectile.Launch(lookDirection, 300);
 
         animator.SetTrigger("Launch");
+
+        PlaySound(throwSound);
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 
 }
